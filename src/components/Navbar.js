971a3ac812d05a99f4d09d562d15b5745ffac515
Erick Rodea <rodeaerick@yahoo.com>
2024-01-27 21:39:16 -0500

    added comments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,23 +4,31 @@ import "materialize-css/dist/css/materialize.min.css";
 import "../css/account.css";
 
 const Navbar = () => {
+  // State to manage the mobile navigation menu
   const [isOpen, setIsOpen] = useState(false);
+
+  // Reference to the navigation container for handling clicks outside
   const navRef = useRef(null);
 
+  // Function to toggle the mobile navigation menu
   const toggleNav = () => {
     setIsOpen(!isOpen);
   };
 
+  // Function to close the mobile navigation menu when a link is clicked
   const handleLinkClick = () => {
     setIsOpen(false);
   };
 
+  // Function to handle clicks outside the navigation container and close the menu
   const handleClickOutside = (event) => {
+    // Close the menu if clicked outside the navigation container
     if (navRef.current && !navRef.current.contains(event.target)) {
       setIsOpen(false);
     }
   };
 
+  // Effect hook to add and remove click event listener for handling clicks outside
   useEffect(() => {
     document.addEventListener("click", handleClickOutside);
 
@@ -45,41 +53,53 @@ const Navbar = () => {
         media="screen,projection"
       />
 
+      {/* Navigation bar with desktop and mobile versions */}
       <nav className="blue" role="navigation">
         <div className="nav-wrapper container" ref={navRef}>
+          {/* Brand logo */}
           <a id="logo-container" href="index.html" className="brand-logo">
             lrnr
           </a>
+
+          {/* Desktop navigation links */}
           <ul className="right hide-on-med-and-down">
             <li>
+              {/* Account link */}
               <a href="/account" onClick={handleLinkClick}>
                 Account
               </a>
             </li>
             <li>
+              {/* Quiz Generation link */}
               <a href="#" onClick={handleLinkClick}>
                 Quiz Generation
               </a>
             </li>
           </ul>
 
+          {/* Mobile navigation menu */}
           <ul
             id="nav-mobile"
             className={`sidenav ${isOpen ? "open" : ""}`}
             style={{
+              // Toggle menu visibility using translateX
               transform: isOpen ? "translateX(0%)" : "translateX(-105%)",
             }}>
             <li>
+              {/* Account link in mobile menu */}
               <a href="/account" onClick={handleLinkClick}>
                 Account
               </a>
             </li>
             <li>
+              {/* Quiz Generation link in mobile menu */}
               <a href="#" onClick={handleLinkClick}>
                 Quiz Generation
               </a>
             </li>
           </ul>
+
+          {/* Mobile menu trigger button */}
           <a
             href="#"
             data-target="nav-mobile"
