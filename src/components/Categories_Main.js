570a6150a2d6a7ae86479fa2639f59fdf8757f12
Erick Rodea <rodeaerick@yahoo.com>
2024-01-29 16:20:19 -0500

    dropdown needs work

--- src/components/Categories_Main.js
+++ src/components/Categories_Main.js
@@ -216,49 +216,244 @@ export default function Categories_Main() {
             </div>
 
             <div className="input-field col s12">
-              <select
-                id="expertise"
-                onChange={handleExpertiseChange}
-                value={expertise}>
-                <option value="" disabled="" />
-                <option value="novice">novice</option>
-                <option value="intermediate">intermediate</option>
-                <option value="expert">expert</option>
-              </select>
+              <div className="select-wrapper">
+                <input
+                  className="select-dropdown dropdown-trigger"
+                  type="text"
+                  readOnly="true"
+                  data-target="select-options-91d287a4-382f-2363-e295-63f8907bb9ec"
+                />
+                <ul
+                  id="select-options-91d287a4-382f-2363-e295-63f8907bb9ec"
+                  className="dropdown-content select-dropdown"
+                  tabIndex={0}
+                  style={{
+                    display: "block",
+                    width: "228.297px",
+                    left: 0,
+                    top: 0,
+                    height: 400,
+                    transformOrigin: "0px 0px",
+                    opacity: 1,
+                    transform: "scaleX(1) scaleY(1)",
+                  }}>
+                  <li
+                    className="disabled selected"
+                    id="select-options-91d287a4-382f-2363-e295-63f8907bb9ec0"
+                    tabIndex={0}>
+                    <span />
+                  </li>
+                  <li
+                    id="select-options-91d287a4-382f-2363-e295-63f8907bb9ec1"
+                    tabIndex={0}>
+                    <span>golang</span>
+                  </li>
+                  <li
+                    id="select-options-91d287a4-382f-2363-e295-63f8907bb9ec2"
+                    tabIndex={0}>
+                    <span>aws</span>
+                  </li>
+                  <li
+                    id="select-options-91d287a4-382f-2363-e295-63f8907bb9ec3"
+                    tabIndex={0}>
+                    <span>javascript</span>
+                  </li>
+                  <li
+                    id="select-options-91d287a4-382f-2363-e295-63f8907bb9ec4"
+                    tabIndex={0}>
+                    <span>CI/CD</span>
+                  </li>
+                  <li
+                    id="select-options-91d287a4-382f-2363-e295-63f8907bb9ec5"
+                    tabIndex={0}>
+                    <span>home gardens</span>
+                  </li>
+                  <li
+                    id="select-options-91d287a4-382f-2363-e295-63f8907bb9ec6"
+                    tabIndex={0}>
+                    <span>coffee</span>
+                  </li>
+                  <li
+                    id="select-options-91d287a4-382f-2363-e295-63f8907bb9ec7"
+                    tabIndex={0}>
+                    <span>finger foods</span>
+                  </li>
+                </ul>
+                <svg
+                  className="caret"
+                  height={24}
+                  viewBox="0 0 24 24"
+                  width={24}
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path d="M7 10l5 5 5-5z" />
+                  <path d="M0 0h24v24H0z" fill="none" />
+                </svg>
+                <select id="topic" tabIndex={-1}>
+                  <option value="" disabled="" selected="" />
+                  <option value="golang">golang</option>
+                  <option value="aws">aws</option>
+                  <option value="javascript">javascript</option>
+                  <option value="ci/cd">CI/CD</option>
+                  <option value="growing you own garden">home gardens</option>
+                  <option value="coffee">coffee</option>
+                  <option value="finger food">finger foods</option>
+                </select>
+              </div>
               <label>Expertise</label>
             </div>
             <div className="input-field col s12">
-              <select
-                id="numquestions"
-                onChange={handleNumberChange}
-                value={number}>
-                <option value="" disabled="" />
-                <option value={5}>5</option>
-                <option value={10}>10</option>
-                <option value={15}>15</option>
-              </select>
+              <div className="select-wrapper">
+                <input
+                  className="select-dropdown dropdown-trigger"
+                  type="text"
+                  readOnly="true"
+                  data-target="select-options-9ce5f51b-888e-8cc2-e1a6-6ba3a95d714d"
+                />
+                <ul
+                  id="select-options-9ce5f51b-888e-8cc2-e1a6-6ba3a95d714d"
+                  className="dropdown-content select-dropdown"
+                  tabIndex={0}
+                  style={{
+                    display: "block",
+                    width: "444.297px",
+                    left: 0,
+                    top: 0,
+                    height: 200,
+                    transformOrigin: "0px 0px",
+                    opacity: 1,
+                    transform: "scaleX(1) scaleY(1)",
+                  }}>
+                  <li
+                    className="disabled"
+                    id="select-options-9ce5f51b-888e-8cc2-e1a6-6ba3a95d714d0"
+                    tabIndex={0}>
+                    <span />
+                  </li>
+                  <li
+                    id="select-options-9ce5f51b-888e-8cc2-e1a6-6ba3a95d714d1"
+                    tabIndex={0}
+                    className="selected">
+                    <span>5</span>
+                  </li>
+                  <li
+                    id="select-options-9ce5f51b-888e-8cc2-e1a6-6ba3a95d714d2"
+                    tabIndex={0}>
+                    <span>10</span>
+                  </li>
+                  <li
+                    id="select-options-9ce5f51b-888e-8cc2-e1a6-6ba3a95d714d3"
+                    tabIndex={0}>
+                    <span>15</span>
+                  </li>
+                </ul>
+                <svg
+                  className="caret"
+                  height={24}
+                  viewBox="0 0 24 24"
+                  width={24}
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path d="M7 10l5 5 5-5z" />
+                  <path d="M0 0h24v24H0z" fill="none" />
+                </svg>
+                <select id="numquestions" tabIndex={-1}>
+                  <option value="" disabled="" />
+                  <option value={5} selected="">
+                    5
+                  </option>
+                  <option value={10}>10</option>
+                  <option value={15}>15</option>
+                </select>
+              </div>
               <label>Number of questions</label>
             </div>
+
             <div className="input-field col s12">
-              <select
-                id="questionstyle"
-                onChange={handleStyleChange}
-                value={style}>
-                <option value="" disabled="" />
-                <option value="master oogway">master oogway</option>
-                <option value="1940's gangster">1940's gangster</option>
-                <option value="like i'm an 8 year old">
-                  like I'm an 8 year old
-                </option>
-                <option value="normal">normal</option>
-                <option value="master yoda">jedi</option>
-                <option value="captain jack sparrow">
-                  captain jack sparrow
-                </option>
-                <option value="matthew mcconaughey">matthew mcconaughey</option>
-              </select>
+              <div className="select-wrapper">
+                <input
+                  className="select-dropdown dropdown-trigger"
+                  type="text"
+                  readOnly="true"
+                  data-target="select-options-72005b65-09d1-16b3-aca8-3e496995585a"
+                />
+                <ul
+                  id="select-options-72005b65-09d1-16b3-aca8-3e496995585a"
+                  className="dropdown-content select-dropdown"
+                  tabIndex={0}
+                  style={{}}>
+                  <li
+                    className="disabled"
+                    id="select-options-72005b65-09d1-16b3-aca8-3e496995585a0"
+                    tabIndex={0}>
+                    <span />
+                  </li>
+                  <li
+                    id="select-options-72005b65-09d1-16b3-aca8-3e496995585a1"
+                    tabIndex={0}>
+                    <span>master oogway</span>
+                  </li>
+                  <li
+                    id="select-options-72005b65-09d1-16b3-aca8-3e496995585a2"
+                    tabIndex={0}>
+                    <span>1940's gangster</span>
+                  </li>
+                  <li
+                    id="select-options-72005b65-09d1-16b3-aca8-3e496995585a3"
+                    tabIndex={0}>
+                    <span>like I'm an 8 year old</span>
+                  </li>
+                  <li
+                    id="select-options-72005b65-09d1-16b3-aca8-3e496995585a4"
+                    tabIndex={0}
+                    className="selected">
+                    <span>normal</span>
+                  </li>
+                  <li
+                    id="select-options-72005b65-09d1-16b3-aca8-3e496995585a5"
+                    tabIndex={0}>
+                    <span>jedi</span>
+                  </li>
+                  <li
+                    id="select-options-72005b65-09d1-16b3-aca8-3e496995585a6"
+                    tabIndex={0}>
+                    <span>captain jack sparrow</span>
+                  </li>
+                  <li
+                    id="select-options-72005b65-09d1-16b3-aca8-3e496995585a7"
+                    tabIndex={0}>
+                    <span>matthew mcconaughey</span>
+                  </li>
+                </ul>
+                <svg
+                  className="caret"
+                  height={24}
+                  viewBox="0 0 24 24"
+                  width={24}
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path d="M7 10l5 5 5-5z" />
+                  <path d="M0 0h24v24H0z" fill="none" />
+                </svg>
+                <select id="questionstyle" tabIndex={-1}>
+                  <option value="" disabled="" />
+                  <option value="master oogway">master oogway</option>
+                  <option value="1940's gangster">1940's gangster</option>
+                  <option value="like i'm an 8 year old">
+                    like I'm an 8 year old
+                  </option>
+                  <option value="normal" selected="">
+                    normal
+                  </option>
+                  <option value="master yoda">jedi</option>
+                  <option value="captain jack sparrow">
+                    captain jack sparrow
+                  </option>
+                  <option value="matthew mcconaughey">
+                    matthew mcconaughey
+                  </option>
+                </select>
+              </div>
               <label>Style of questions</label>
             </div>
+
             <div className="input-field col s12">
               <button
                 id="btn-categories"
