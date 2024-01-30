590af30627d1405da46baee8d622cfd7048028f2
Erick Rodea <rodeaerick@yahoo.com>
2024-01-30 14:20:50 -0500

    quiz page done need API calls

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -3,13 +3,14 @@ import Account_Main from "./components/Account_Main";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Quiz_Main from "./components/Quiz_Main";
 import "./css/account.css";
 
 export default function Quiz() {
   return (
     <div>
       <Navbar />
-      {/* <Quiz_Main /> */}
+      <Quiz_Main />
       <Footer />
     </div>
   );
