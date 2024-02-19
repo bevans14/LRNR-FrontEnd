bf8c4383a2005d54317ad002cab980c71be89345
bevans14 <146108421+bevans14@users.noreply.github.com>
2024-02-19 10:39:52 -0500

    first commit

---
+++ cypress/e2e/FirstTest.cy.js
@@ -0,0 +1,21 @@
+describe('Homepage', () => {
+  it('Loads the homepage successfully', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('Your guided path to programming enlightenment') 
+  })})
+
+  it('Displays featured content', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('.banner').should('exist') 
+  })
+
+  it('Has a footer', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('footer').should('exist') // Replace 'footer' with your footer selector
+  })
+
+  it('Displays featured content', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('.banner img').should('exist') 
+  })
+
